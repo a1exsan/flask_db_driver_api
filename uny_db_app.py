e9bb3ad9e61ee05b7636adc925584f0445b31d29
alex <gominsan@gmail.com>
2024-12-11 21:42:05 +0700

    Initial commit

---
+++ uny_db_app.py
@@ -0,0 +1,158 @@
+from fileinput import filename
+
+from flask import Flask, request
+import uny_db_driver
+import json
+import logging
+import base64
+
+app = Flask(__name__)
+
+#logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+def home():
+    return 'UNY_DB_APP'
+
+@app.route('/get_info/<db_name>')
+def get_db_info(db_name):
+    db = uny_db_driver.uny_litebase(db_name)
+    tabs = db.get_all_tables_name()
+    tabs = [i[0] for i in tabs]
+    info = {}
+    for tab in tabs:
+        record_number = db.get_all_tab_data(tab)
+        cols = db.get_table_col_names(tab)
+        info[tab] = cols
+        info[f'{tab}_records'] = len(record_number)
+    if info != None:
+        return info, 200
+    return 'Server error', 404
+
+@app.route('/get_info/<db_name>/<tab_name>')
+def get_db_tab_info(db_name, tab_name):
+    db = uny_db_driver.uny_litebase(db_name)
+    cols = db.get_table_col_names(tab_name)
+    if cols != None:
+        return cols, 200
+    return 'Server error', 404
+
+@app.route('/get_all_tab_data/<db_name>/<tab_name>')
+def get_all_tab_data(db_name, tab_name):
+    db = uny_db_driver.uny_litebase(db_name)
+    data = db.get_all_tab_data(tab_name)
+    if data != None:
+        return data, 200
+    return 'Server error', 404
+
+@app.route('/get_keys_data/<db_name>/<tab_name>/<item>/<value>')
+def get_keys_data(db_name, tab_name, item, value):
+    db = uny_db_driver.uny_litebase(db_name)
+    data = db.get_all_tab_data_by_keys(tab_name, item, value)
+    if data != None:
+        return data, 200
+    return 'Server error', 404
+
+@app.route('/insert_data/<db_name>/<tab_name>', methods=['POST'])
+def insert_data(db_name, tab_name):
+
+    #r = requests.post('http://127.0.0.1:8881/insert_data/test_1.db/main_tab',
+    # json=json.dumps(['test_name','test_content']))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    data = json.loads(request.json)
+    cursor, status = db.insert_data(tab_name, data)
+    if status == 'ok':
+        return status, 200
+    return 'Server error', 404
+
+@app.route('/update_data/<db_name>/<tab_name>/<record_id>', methods=['PUT'])
+def update_data(db_name, tab_name, record_id):
+
+    #r = requests.put('http://127.0.0.1:8881/update_data/test_1.db/main_tab/10',
+    # json=json.dumps({'name_list':['name', 'text'],'value_list':['test_name','CONTENT111']}))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    data = json.loads(request.json)
+    cursor, status = db.update_data(tab_name, record_id, data['name_list'], data['value_list'])
+    if status == 'ok':
+        return status, 200
+    #app.logger.info(status)
+    return 'Server error', 404
+
+@app.route('/delete_data/<db_name>/<tab_name>/<record_id>', methods=['DELETE'])
+def delete_data(db_name, tab_name, record_id):
+
+    #r = requests.put('http://127.0.0.1:8881/update_data/test_1.db/main_tab/10',
+    # json=json.dumps({'name_list':['name', 'text'],'value_list':['test_name','CONTENT111']}))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    status = db.delete_row(tab_name, record_id)
+    if status == 'ok':
+        return status, 200
+    return 'Server error', 404
+
+@app.route('/insert_file_data/<db_name>/<tab_name>', methods=['POST'])
+def insert_file_data(db_name, tab_name):
+
+    #r = requests.post('http://127.0.0.1:8881/insert_file_data/map_analytics_1.db/lcms_tab',
+    # json=json.dumps({'filename': 'test_file.txt', 'content': 'hWBCQUWEBC', 'to_base': [1, 2, 'A1', 'test_file.txt']}))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    data = json.loads(request.json)
+
+    if data['content'] != '':
+        cursor, status = db.insert_data(tab_name, data['to_base'])
+        with open(f"lcms_files/{data['filename']}", 'w') as f:
+            f.write(data['content'])
+    else:
+        status = 'None'
+
+    if status == 'ok':
+        return status, 200
+    return 'Server error', 404
+
+@app.route('/get_file_data/<db_name>/<tab_name>/<id>/<order_id>/<pos>', methods=['GET'])
+def get_file_data(db_name, tab_name, id, order_id, pos):
+
+    #r = requests.post('http://127.0.0.1:8881/insert_file_data/map_analytics_1.db/lcms_tab',
+    # json=json.dumps({'filename': 'test_file.txt', 'content': 'hWBCQUWEBC', 'to_base': [1, 2, 'A1', 'test_file.txt']}))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    results = db.get_all_tab_data_by_keys(tab_name, 'filename',f'{id}_{order_id}_{pos}.mzdata.xml')
+
+    if len(results) > 0:
+        filename_ = results[-1][4]
+    else:
+        filename_ = ''
+
+    #data = json.loads(request.json)
+    #cursor, status = db.insert_data(tab_name, data['to_base'])
+
+    if filename_ != '':
+        with open(f"lcms_files/{filename_}", 'r') as f:
+            data = f.read()
+    else:
+        data = None
+
+    if data is not None:
+        return {'file_content': data, 'tag_tab': results[-1][5]}, 200
+    return 'Server error', 404
+
+@app.route('/update_file_data/<db_name>/<tab_name>/<record_id>', methods=['PUT'])
+def update_file_data(db_name, tab_name, record_id):
+
+    #r = requests.put('http://127.0.0.1:8881/update_data/test_1.db/main_tab/10',
+    # json=json.dumps({'name_list':['name', 'text'],'value_list':['test_name','CONTENT111']}))
+
+    db = uny_db_driver.uny_litebase(db_name)
+    data = json.loads(request.json)
+    cursor, status = db.update_data(tab_name, record_id, data['name_list'], data['value_list'])
+    if status == 'ok':
+        return status, 200
+    #app.logger.info(status)
+    return 'Server error', 404
+
+
+if __name__ == '__main__':
+    app.run(port=8881, debug=True)
---
+++ uny_db_driver.py
@@ -0,0 +1,232 @@
+import sqlite3
+import pandas as pd
+import json
+
+class uny_litebase():
+    def __init__(self, base_name):
+        self.base_name = base_name
+        self.tables = {}
+
+    def dict_to_text(self, dd):
+        return json.dumps(dd)
+
+    def text_to_dict(self, text):
+        #print(text)
+        return json.loads(text)
+
+    def df_to_text(self, dataframe):
+        return json.dumps(dataframe.to_dict('records'))
+
+    def text_to_df(self, text):
+        return pd.DataFrame(json.loads(text))
+
+    def add_item(self, tab_name, item_name, item_type):
+        if tab_name not in list(self.tables.keys()):
+            self.tables[tab_name] = {'items': [], 'types': []}
+
+        self.tables[tab_name]['items'].append(item_name)
+        self.tables[tab_name]['types'].append(item_type)
+
+    def create_tables(self):
+        db = sqlite3.connect(self.base_name)
+        cursor = db.cursor()
+
+        for tab_name in list(self.tables.keys()):
+            #print(tab_name)
+            s = 'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
+            for item_, type_ in zip(self.tables[tab_name]['items'], self.tables[tab_name]['types']):
+                s += f'{item_} {type_}, '
+            #print(s)
+            #print(s[:-2])
+            cursor.execute(f"""DROP TABLE IF EXISTS {tab_name}""")
+            #print(f""" CREATE TABLE {tab_name} ({s[:-2]}) """)
+            cursor.execute(f""" CREATE TABLE {tab_name} ({s[:-2]}) """)
+
+        db.commit()
+        db.close()
+
+    def get_all_tables_name(self):
+        db = sqlite3.connect(self.base_name)
+        cursor = db.cursor()
+        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+        results = cursor.fetchall()
+        db.close()
+        return results
+
+
+    def get_table_col_names(self, tab_name):
+        db = sqlite3.connect(self.base_name)
+        cursor = db.cursor()
+        cursor.execute(f'SELECT * FROM {tab_name}')
+        db.close()
+        return [description[0] for description in cursor.description]
+
+    def insert_data(self, tab_name, value_list):
+        status = 'ok'
+        try:
+            connection = sqlite3.connect(self.base_name)
+            cursor = connection.cursor()
+
+            s = f'INSERT INTO {tab_name} ( '
+            t = ' VALUES ('
+            for value, column in zip(value_list, self.get_table_col_names(tab_name)[1:]):
+                s += f"{column}, "
+                t += f"?, "
+            s, t = s[:-2] + ')', t[:-2] + ')'
+            #print(s+t)
+            cursor.execute(s + t, tuple(value_list))
+
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            status = f'error {e}'
+        finally:
+            connection.close()
+        return cursor.lastrowid, status
+
+    def control_exist(self, tab_name, field_name, value):
+        connection = sqlite3.connect(self.base_name)
+        cursor = connection.cursor()
+
+        cursor.execute(f'SELECT * FROM {tab_name} WHERE {field_name} = ?', [value])
+
+        results = cursor.fetchall()
+        connection.close()
+        if len(results) > 0:
+            return True
+        else:
+            return False
+
+    def update_data(self, tab_name, id, name_list, value_list):
+        status = 'ok'
+        try:
+            connection = sqlite3.connect(self.base_name)
+            cursor = connection.cursor()
+
+            s = f'UPDATE {tab_name} SET '
+            for value, column in zip(value_list, name_list):
+                s += f"{column} = ?, "
+            s = s[:-2]
+            s += 'WHERE id = ?'
+            #print(s+t)
+            value_list.append(id)
+            cursor.execute(s, tuple(value_list))
+
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            status = f'error {e}'
+        finally:
+            connection.close()
+        return cursor.lastrowid, status
+
+    def delete_row(self, tab_name, id):
+        status = 'ok'
+        try:
+            connection = sqlite3.connect(self.base_name)
+            cursor = connection.cursor()
+
+            s = f'DELETE FROM {tab_name} WHERE id = ?'
+            cursor.execute(s, (id,))
+
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            status = f'error {e}'
+        finally:
+            connection.close()
+        return status
+
+    def get_all_tab_data(self, tab_name):
+        connection = sqlite3.connect(self.base_name)
+        cursor = connection.cursor()
+
+        cursor.execute(f'SELECT * FROM {tab_name}')
+
+        results = cursor.fetchall()
+        connection.close()
+
+        return results
+
+    def get_all_tab_data_by_keys(self, tab_name, item_name, item_value):
+        connection = sqlite3.connect(self.base_name)
+        cursor = connection.cursor()
+
+        cursor.execute(f'SELECT * FROM {tab_name} WHERE {item_name} = ?', [item_value])
+
+        results = cursor.fetchall()
+        connection.close()
+
+        return results
+
+
+def create(tab_name):
+    db = sqlite3.connect('test_1.db')
+    cursor = db.cursor()
+
+    cursor.execute(f"""DROP TABLE IF EXISTS {tab_name}""")
+    cursor.execute(f""" CREATE TABLE {tab_name}(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    	pos_name VARCHAR(255),
+    	unicode VARCHAR(255),
+    	units VARCHAR(255),
+    	description text,
+    	lower_limit INTEGER
+    )
+        """)
+
+    db.commit()
+    db.close()
+
+def test_run():
+    db = uny_litebase('test_2.db')
+
+    #db.add_item('first_tab', 'name', 'VARCHAR(255)')
+    #db.add_item('first_tab', 'number', 'INTEGER')
+    #db.add_item('first_tab', 'amount', 'FLOAT')
+    #db.add_item('first_tab', 'description', 'text')
+
+    #db.add_item('second_tab', 'name', 'VARCHAR(255)')
+    #db.add_item('second_tab', 'number', 'INTEGER')
+    #db.add_item('second_tab', 'amount', 'VARCHAR(255)')
+
+    #db.create_tables()
+
+    #print(db.get_table_col_names('first_tab'))
+    db.insert_data('first_tab', ['name1', 1, 2.44, 'abracadabra 1'])
+    db.insert_data('first_tab', ['name2', 2, 3.14, 'abracadabra 2'])
+    db.insert_data('second_tab', ['text 2', 10000, 'abracadabra 2'])
+    db.insert_data('second_tab', ['ijbcuhjkedce', 23423, 'hello world'])
+
+    for row in db.get_all_tab_data('first_tab'):
+        print(row)
+
+    for row in db.get_all_tab_data('second_tab'):
+        print(row)
+
+def show_all_tabs():
+    db = uny_litebase('map_analytics_1.db')
+    print(db.get_table_col_names('lcms_tab'))
+    data = db.get_all_tab_data('lcms_tab')
+    for r in data:
+        print(r)
+
+def create_file_base():
+    db = uny_litebase('map_analytics_1.db')
+
+    db.add_item('lcms_tab', 'map_id', 'INTEGER')
+    db.add_item('lcms_tab', 'order_id', 'INTEGER')
+    db.add_item('lcms_tab', 'map_position', 'VARCHAR(255)')
+    db.add_item('lcms_tab', 'filename', 'VARCHAR(255)')
+    db.add_item('lcms_tab', 'tags', 'VARCHAR(255)')
+
+    #db.add_item('main_tab', 'chrom_filename', 'VARCHAR(255)')
+    #db.add_item('main_tab', 'report_filename', 'VARCHAR(255)')
+
+    db.create_tables()
+
+
+if __name__ == '__main__':
+    #create('test')
+    show_all_tabs()
+    #create_file_base()
